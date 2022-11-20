97e33e24715e2d8fa590ed0487e4241dec87e3ac
CoderDuMin <1325964835@qq.com>
2022-11-20 21:51:42 +0800

    聊天室改版

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ io.on('connection',socket => {
   console.log(socket.handshake.query.username+'进入了聊天室',socket.handshake.query.username)
   socket.on('chat message',msg=>{
     console.log(msg)
-    io.emit('chat message',msg)
+    io.emit('chat message',{name:socket.handshake.query.username,msg})
   })
   socket.on('disconnect',()=>{
     console.log('这个人离开了聊天室');
   })
 })
 
-server.listen('3939',()=>{
-  console.log('服务开启了');
+server.listen('5504',()=>{
+  console.log('聊天室服务开启在5504了');
 })
